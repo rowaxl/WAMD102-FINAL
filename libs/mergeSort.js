a8647db211e6b2c070d49d5f1bd80b48b0b576bd
rowaxl <rowaxl0@gmail.com>
2020-06-03 15:49:27 -0700

    connect with visualizer

---
+++ libs/mergeSort.js
@@ -0,0 +1,41 @@
+function merge(left, right) {
+  const merged = [];
+
+  // compare elements of two arrays in same index (do while left.length == right.length)
+  while (left.length && right.length) {
+    if (left[0] <= right[0]) {
+      merged.push(left[0]);
+      left.shift();
+    } else {
+      merged.push(right[0]);
+      right.shift();
+    }
+  }
+
+  // push left elements of left array(if left.length > right.length)
+  while (left.length) {
+    merged.push(left.shift());
+  }
+
+  // push left elements of right array(if left.length < right.length)
+  while (right.length) {
+    merged.push(right.shift());
+  }
+
+  return merged;
+}
+
+function mergeSort(numbers) {
+  if (numbers.length < 2) {
+    return numbers;
+  }
+
+  // split arrays into two part
+  const left = numbers.slice(0, Math.floor(numbers.length/ 2));
+  const right = numbers.slice(Math.floor(numbers.length/ 2), numbers.length);
+
+  // sort and merge the elements recursively
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+module.exports = { mergeSort };
