55e293cfb5b1fe04933d1e145ed36cbc57921873
rowaxl <rowaxl0@gmail.com>
2020-06-04 13:48:51 -0700

    bubble sort and insertion sort

---
+++ libs/bubleSort.js
@@ -0,0 +1,20 @@
+// Bubble Sort
+const numbers = [45, 42, 16, 4, 1, 56, 93, 87, 286];
+
+function bubbleSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) {
+      if (array[j] > array[j + 1]) {
+        // swap two number's index
+        let temp = array[j]
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
+}
+
+bubbleSort(numbers);
+console.log(numbers);
